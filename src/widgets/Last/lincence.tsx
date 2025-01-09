ac99b0e81db6789475054da59c04e4d7802906fb
vladi <vladimir.seleznev.02@mail.ru>
2025-01-09 05:59:51 +0400

    header for ssr and licence onclick

--- src/widgets/Last/lincence.tsx
+++ src/widgets/Last/lincence.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react';
 import styles from './licence.module.css'
 import { handleDownloadDoc } from "src/feature/download-prez";
--- src/widgets/global/components/header.tsx
+++ src/widgets/global/components/header.tsx
@@ -1,4 +1,5 @@
-'use client'
+'use client';
+
 import React, { useState, useEffect } from "react";
 import { FaWhatsappSquare, FaPhoneSquareAlt } from "react-icons/fa";
 import { TransparentButton } from "src/shareds/button/button";
@@ -6,25 +7,30 @@ import styles from '../styles/header.module.css';
 import flex from '../../../shareds/styles/flex.module.css';
 import handleWhatsAppRedirect from "src/feature/whats-app";
 
-
 const Header: React.FC = () => {
   const [isActivePopUp, setIsActivePopUp] = useState(false);
-  const [isMobileView, setIsMobileView] = useState(window.innerWidth <= 450);
-  const [isActivePhone, setIsActivePhone] = useState(false)
+  const [isMobileView, setIsMobileView] = useState(false); // Изначально false
+  const [isActivePhone, setIsActivePhone] = useState(false);
+
   useEffect(() => {
     const handleResize = () => {
       setIsMobileView(window.innerWidth <= 450);
     };
 
+    handleResize();
+
     window.addEventListener("resize", handleResize);
+
     return () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
-  const handlePhone =()=>{
-    setIsActivePhone((prev)=>!prev)
-  }
-    const handlePopUp = () => {
+
+  const handlePhone = () => {
+    setIsActivePhone((prev) => !prev);
+  };
+
+  const handlePopUp = () => {
     setIsActivePopUp((prev) => !prev);
   };
 
@@ -33,16 +39,25 @@ const Header: React.FC = () => {
       <h1>Engineering Services</h1>
       <nav className={styles.callContainer}>
         {isMobileView ? (
-          <div/>
+          <div /> // Пустой контейнер для мобильной версии
         ) : (
           <TransparentButton currentClick={handlePopUp} label="Заказать обработанный звонок" />
         )}
-        <div className={isActivePopUp ? styles.active : `${styles.inactive} ${flex.flexColumn}`}>
+        <div
+          className={isActivePopUp ? styles.active : `${styles.inactive} ${flex.flexColumn}`}
+        >
           <div className={`${styles.contactServices} ${flex.flexRow}`}>
-            <FaWhatsappSquare className={styles.whatsappIcon} onClick={handleWhatsAppRedirect}/>
-            <FaPhoneSquareAlt className={styles.phoneIcon} onClick={handlePhone}/>
+            <FaWhatsappSquare
+              className={styles.whatsappIcon}
+              onClick={handleWhatsAppRedirect}
+            />
+            <FaPhoneSquareAlt className={styles.phoneIcon} onClick={handlePhone} />
           </div>
-          {isActivePhone?<span>+7(919) 876-54-32</span>:<div/>}
+          {isActivePhone ? (
+            <span>+7(919) 876-54-32</span>
+          ) : (
+            <div /> // Пустой элемент для ненужного отображения
+          )}
         </div>
       </nav>
     </header>
